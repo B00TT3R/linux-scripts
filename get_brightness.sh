3ac23321171c137fb573aaa9b7639c1228d81e76
Bot_Zero <boott3r@gmail.com>
2024-10-25 23:20:52 -0400

    troca todos os scripts pra lua

--- get_brightness.sh
+++ get_brightness.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Fetch current brightness level and format as a percentage
 current_brightness=$(xbacklight -get | awk '{printf "%.0f", $1}')
-
-# Output the percentage value
 echo "${current_brightness}%"
 
